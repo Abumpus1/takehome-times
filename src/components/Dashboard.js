f495f9b6254a02e4528e3c7c49524d072a7b091a
Alex Bumpus <alexander.bumpus@yahoo.com>
2022-07-27 13:38:12 -0600

    Set up initial layout for item cards

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,13 +1,19 @@
 import React from "react";
 import ItemCard from "./ItemCard";
+import "../styles/Dashboard.css";
 
 const Dashboard = () => {
   return (
-    <section>
+    <section className="dashboard">
       <ItemCard />
       <ItemCard />
       <ItemCard />
       <ItemCard />
+      <ItemCard />
+      <ItemCard />
+      <ItemCard />
+      <ItemCard />
+      
     </section>
   )
 }
--- src/components/ItemCard.js
+++ src/components/ItemCard.js
@@ -1,9 +1,16 @@
 import React from "react";
+import "../styles/ItemCard.css";
 
 const ItemCard = () => {
   return (
-    <article>
-      CARD
+    <article className="item-card">
+      <div className="thumbnail">
+        <p>IMAGE IMAGE IMAGE IMAGE</p>
+      </div>
+      <div className="card-info">
+        <h3>Title explaining this article</h3>
+        <p>2022-07-27</p>
+      </div>
     </article>
   )
 }
