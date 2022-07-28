c1a6f8577c47731603a80b6cd707ce078798dac8
Alex Bumpus <alexander.bumpus@yahoo.com>
2022-07-27 21:54:08 -0600

    Connect story data to Detailed View, improve error handling

--- src/components/DetailedView.js
+++ src/components/DetailedView.js
@@ -1,9 +1,14 @@
 import React from "react";
+import { useParams } from "react-router-dom";
 
-const DetailedView = () => {
+const DetailedView = ({ stories }) => {
+
+  const { id } = useParams();
+  const storyData = stories.find(story => story.id === id)
+
   return (
-    <section>
-      DETAILED VIEW
+    <section className="detailed-view">
+      <h2>{storyData.title}</h2>
     </section>
   )
 }
